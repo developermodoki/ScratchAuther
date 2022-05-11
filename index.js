6ed5044bffd96d8f64c6ec2d7c4f5a97cb52121b
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-05-11 18:10:23 +0900

    Update

--- index.js
+++ index.js
@@ -13,12 +13,10 @@ require("dotenv").config();
 
 client.on("ready", bot => {
   console.log(`Logged in as ${client.user.tag}.`);
-  bot.user.setActivity(`${client.ws.ping}ms | https://scratch.mit.edu/projects/673753313/`)
+  setTimeout(() => bot.user.setActivity(`${client.ws.ping}ms | https://scratch.mit.edu/projects/673753313/`), 5000);
 });
 
-setInterval(() => {
-  client.user.setActivity(`${client.ws.ping}ms | https://scratch.mit.edu/projects/673753313/`)
-},1000 * 60 * 5)
+setInterval(() => client.user.setActivity(`${client.ws.ping}ms | https://scratch.mit.edu/projects/673753313/`), 1000 * 60 * 5);
 
 client.on("messageCreate", (message) => {
   if (message.content === "!scratchauth" && message.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
