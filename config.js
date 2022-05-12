020c7aac6d22309152938688cea201eff9cdd7da
developermodoki <61893884+developermodoki@users.noreply.github.com>
2022-05-12 16:10:31 +0900

    fix bugs

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "verifiedRoles": ["123456789012345678"],
-  "loggingChannel": "123456789012345678",
+  "verifiedRoles": ["892320750940332033"],
+  "loggingChannel": "932145437715075092",
   "logging": ["scratch.username", "discord.tag", "discord.id", "uuid"]
 }
